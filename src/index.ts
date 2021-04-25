114adab3db3d86cd6c82a8b5ec75110b1f03ddcd
envy124 <pkgsail.dev@gmail.com>
2021-04-25 13:49:30 +0300

    init

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import cors from 'cors';
+import path from 'path';
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'colyseus';
+import { GameRoom } from './room';
+
+const port = Number(process.env.PORT || 3000) + Number(process.env.NODE_APP_INSTANCE || 0);
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/', express.static(path.join(__dirname, "public")));
+
+// Attach WebSocket Server on HTTP Server.
+const gameServer = new Server({
+  server: createServer(app),
+  express: app,
+  pingInterval: 0,
+});
+
+gameServer.define("game", GameRoom)
+    .enableRealtimeListing();
+
+gameServer.onShutdown(() => {
+    console.log(`game server is going down.`);
+});
+  
+gameServer.listen(port);
+  
+console.log(`Listening on http://localhost:${ port }`);
