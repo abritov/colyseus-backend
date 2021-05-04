967e2311a3ed6ba29b450da7b07f3f316ffe1b5d
envy124 <pkgsail.dev@gmail.com>
2021-05-04 14:11:54 +0300

    add phaser typings

---
+++ typings/custom.d.ts
@@ -0,0 +1,12 @@
+declare namespace Phaser {
+    namespace Physics {
+        namespace Matter {
+            export const Matter: any
+            export const matter: any
+        }
+    }
+}
+  
+interface Window {
+    game: Phaser.Game
+}
